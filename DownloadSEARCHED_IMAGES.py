7d418b99b4b2b1b1548629be0d6de7d3f027c77c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-08-16 13:19:04 +0200

    Cretaed py instance

---
+++ DownloadSEARCHED_IMAGES.py
@@ -0,0 +1,41 @@
+import requests
+import io
+import os
+
+from bs4 import BeautifulSoup
+
+
+def search_google_images(query):
+    url = "https://www.google.com/search?q={}".format(query)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    image_results = soup.find_all("img", class_="rg_i")
+    image_urls = [image["src"] for image in image_results]
+
+    return image_urls
+
+
+def download_images(image_urls, output_directory):
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+
+    for image_url in image_urls:
+        image_name = image_url.split("/")[-1]
+        image_path = os.path.join(output_directory, image_name)
+
+        response = requests.get(image_url)
+        with open(image_path, "wb") as f:
+            f.write(response.content)
+
+
+def main():
+    query = "Aalto House"
+    output_directory = "aalto_house_images"
+
+    image_urls = search_google_images(query)
+    download_images(image_urls, output_directory)
+
+
+if __name__ == "__main__":
+    main()
